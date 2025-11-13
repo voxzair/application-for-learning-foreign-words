a35eb30ebc64c9c918f56548ea2164f71b48a2f6
voxzair <zairjack@gmail.com>
2025-11-13 10:46:54 +0700

    Добавьте файлы проекта.

---
+++ application for learning foreign words_UI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace application_for_learning_foreign_words_UI
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
